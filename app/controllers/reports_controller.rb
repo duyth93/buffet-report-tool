158cd821d19f448f9d0e3b7a15b6c0ec1444acb5
duyth93 <tang.hoai.duy@framgia.com>
2018-07-01 00:38:07 +0700

    Update

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,4 @@
 class ReportsController < ApplicationController
-  before_action :get_chatwork_api, only: [:create, :new]
-
   def index
   end
 
@@ -30,9 +28,5 @@ class ReportsController < ApplicationController
   def report_params
     params.require(:report).permit(:problems, :next_day_plan, :free_comment, :room_id, :to_id, :to_name,
       :template_id, report_details_attributes: [:task, :actual, :percent]).merge(user_id: current_user.id)
-  end
-
-  def get_chatwork_api
-    @chatwork_api = ChatworkApi.first
   end
 end
